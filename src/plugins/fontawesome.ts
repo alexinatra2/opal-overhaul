0c78ad8532c3f560fa4c8634cceb59f549ae5818
alexinatra2 <a.holzknecht@gmx.de>
2025-01-23 11:32:22 +0100

    add fontawesome icons

---
+++ src/plugins/fontawesome.ts
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+// Add icons to the library (you can add more here)
+library.add(faBars);
+
+// Export the FontAwesome component for global use
+export default FontAwesomeIcon;
