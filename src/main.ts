06729e90fad7c1e9faabe480b09a012a5b03c93e
alexinatra2 <a.holzknecht@gmx.de>
2025-01-23 16:06:26 +0100

    remove fontawesome plugin

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import FontAwesomeIcon from "./plugins/fontawesome";
 
 const app = createApp(App);
 
-app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 
 app.mount("#app");
