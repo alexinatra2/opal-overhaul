0dd06ed5a3a991bcdf089da7de7bbc6827ccb9f3
alexinatra2 <a.holzknecht@gmx.de>
2025-01-23 16:53:21 +0100

    update router

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,31 @@
 import { createRouter, createWebHistory, RouterOptions } from "vue-router";
+import coursesStore from "./store/courses";
+import Course from "./components/course/Course.vue";
+import NoCoursesStartPage from "./components/start/NoCoursesStartPage.vue";
+import CoursesStartPage from "./components/start/CoursesStartPage.vue";
 
-const routes: RouterOptions["routes"] = [];
+const routes: RouterOptions["routes"] = [
+  { path: "/courses", component: NoCoursesStartPage },
+  { path: "/no-courses", component: CoursesStartPage },
+  { path: "/courses/:id", component: Course },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Navigation Guard to Redirect Based on Course Availability
+router.beforeEach((to, _from, next) => {
+  if (to.path === "/") {
+    if (coursesStore.courses.length > 0) {
+      next("/courses"); // Redirect to Course Overview if user has courses
+    } else {
+      next("/first-time"); // Redirect to FirstTime if user has no courses
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
