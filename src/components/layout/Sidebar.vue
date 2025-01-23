907b7975e6375c2d9aa56623e7ebbfacb308d404
alexinatra2 <a.holzknecht@gmx.de>
2025-01-23 16:06:16 +0100

    refactor Sidebar

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { defineProps, defineEmits } from "vue";
+import coursesStore from "@/store/courses";
+import SidebarCourse from "./SidebarCourse.vue";
 
 defineProps<{ isOpen: boolean }>();
 const emit = defineEmits(["close"]);
@@ -8,9 +10,12 @@ const emit = defineEmits(["close"]);
 <template>
   <div class="backdrop" v-if="isOpen" @click="emit('close')"></div>
   <aside class="drawer" :class="{ open: isOpen }">
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+    <nav class="nav">
+      <ul class="course-list">
+        <li v-for="course in coursesStore.courses" :key="course.id">
+          <SidebarCourse :name="course.name" :id="course.id" :active="coursesStore.activeCourse === course.id"/>
+        </li>
+      </ul>
     </nav>
   </aside>
 </template>
@@ -18,13 +23,13 @@ const emit = defineEmits(["close"]);
 <style scoped>
 .drawer {
   position: fixed;
-  top: 0;
-  left: -250px;
-  width: 250px;
-  height: 100vh;
-  background: var(--background-dark);
-  color: white;
-  padding: 1rem;
+  top: 3.5rem;
+  bottom: 2rem;
+  left: -300px;
+  width: 300px;
+  z-index: 200;
+  height: calc(100vh - 6rem);
+  background: var(--main-theme-color-60);
   transition: left 0.3s ease;
 }
 
@@ -32,9 +37,19 @@ const emit = defineEmits(["close"]);
   left: 0;
 }
 
+.course-list { 
+  width: 100%;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  list-style: none;
+}
+
 .backdrop {
   position: fixed;
   inset: 0;
+  z-index: 50;
   background: rgba(0, 0, 0, 0.5);
 }
 </style>
