f3871e30fddc2e1442f937e0adf245fa910a1890
alexinatra2 <a.holzknecht@gmx.de>
2025-01-23 16:08:16 +0100

    update footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <footer class="footer">
     <div class="footer-content">
@@ -12,39 +8,30 @@
   </footer>
 </template>
 
-
-
 <style scoped>
 .footer {
-  position: fixed;
-  bottom: 0;
-  left: 0;
   width: 100%;
-  height: 30px; /* Höhe des Footers */
-  background-color: var(--background-grey);
-  color: var(--text-light);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1); /* Schatten oben */
+  background-color: var(--main-theme-gray-70);
+  box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1); 
 }
 
 .footer-content {
   display: flex;
+  justify-content: flex-end;
   align-items: center;
-  gap: 2em; /* Abstand zwischen den Links */
+  padding: 0.5rem 1rem;
+  gap: 2rem; 
 }
 
 .footer-link {
-  text-decoration: none; /* Entfernt die Standard-Unterstreichung */
-  color: var(--text-light); /* Setzt die Linkfarbe */
-  font-size: 0.9rem; /* Optionale Schriftgröße */
-  transition: text-decoration 0.3s ease, color 0.3s ease; /* Sanfter Übergang */
+  text-decoration: none; 
+  color: var(--text-dark); 
+  transition: text-decoration 0.3s ease, color 0.3s ease; 
 }
 
 .footer-link:hover {
-  text-decoration: underline; /* Unterstreicht den Text bei Hover */
-  color: var(--main-theme-color-40); /* Ändert die Farbe beim Hover */
+  text-decoration: underline;
+  color: var(--main-theme-color-40);
 }
 </style>
 
