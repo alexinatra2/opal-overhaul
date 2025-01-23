149b30e20517c989391f66696e59c91ac72126cb
alexinatra2 <a.holzknecht@gmx.de>
2025-01-23 16:06:08 +0100

    add SidebarCourse component

---
+++ src/components/layout/SidebarCourse.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface Props { 
+  name: string;
+  id?: string;
+  active?: boolean;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <router-link to="/" class="link">
+    <li class="item" :class="{ 'active': props.active }">
+      <h4>{{ props.name }}</h4>
+      <p class="course-id">{{ props.id }}</p>
+    </li>
+  </router-link>
+</template>
+
+<style scoped>
+.item {
+  width: 300px;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+  list-style: none;
+  color: var(--text-dark);
+  transition: background-color 0.3s ease;
+}
+
+.item:hover {
+  background-color: var(--main-theme-color-50);
+}
+
+.active {
+  background-color: var(--main-theme-color-);
+}
+
+.link {
+  text-decoration: none;
+}
+
+.course-id { 
+  color: var(--main-theme-gray-20);
+}
+</style>
