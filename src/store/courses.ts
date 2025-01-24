44ad2afe0dae6886ce676db600b9307e17eeadc8
alexinatra2 <a.holzknecht@gmx.de>
2025-01-24 16:48:46 +0100

    add enrolled and nodes field to course type

--- src/store/courses.ts
+++ src/store/courses.ts
@@ -1,10 +1,17 @@
 import {defineStore} from "pinia";
 
+export type CourseNode = {
+    title: string;
+    children?: CourseNode[];
+}
+
 export interface Course {
     name: string;
     id: string;
     author: string;
     desc?: string;
+    enrolled?: boolean;
+    nodes?: CourseNode[];
 }
 
 const useCoursesStore = defineStore("courses", () => ({
@@ -14,6 +21,34 @@ const useCoursesStore = defineStore("courses", () => ({
             id: "II-903",
             desc: "Dies ist ein Beispielkurs mit kurzer Beschreibung",
             author: "Prof. Freitag",
+            enrolled: true,
+            nodes: [
+                {
+                    title: "Skript",
+                    children: [
+                        {
+                            title: "Teil 1",
+                        },
+                        {
+                            title: "Teil 2",
+                        },
+                    ]
+                },
+                {
+                    title: "Aufgaben",
+                    children: [
+                        {
+                            title: "Teil 1",
+                        },
+                        {
+                            title: "Teil 2",
+                        },
+                    ]
+                },
+                {
+                    title: "Lösungen",
+                },
+            ]
         },
         {
             name: "Kurs 2",
