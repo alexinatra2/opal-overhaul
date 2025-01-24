6ad74b32181158ae3a8bbfff46d427b7218f9823
alexinatra2 <a.holzknecht@gmx.de>
2025-01-24 21:51:23 +0100

    implement file tree

---
+++ src/components/course/CourseContent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {Course} from "@/store/courses.ts";
+
+defineProps<{ course: Course }>();
+</script>
+
+<template>
+  <section class="w-full h-full bg-red-500 p-4">
+    <h1 class="text-3xl font-bold">{{ course.name }}</h1>
+  </section>
+</template>
---
+++ src/components/course/FileTree.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {CourseNode} from "@/store/courses.ts";
+import FileTreeNode from "@/components/course/FileTreeNode.vue";
+
+withDefaults(
+    defineProps<{ nodes: CourseNode[]; depth?: number }>(),
+    {
+      nodes: () => [],
+      depth: 0
+    }
+);
+</script>
+
+<template>
+  <div class="w-full">
+    <ul>
+      <FileTreeNode v-for="node in nodes" :key="node.title" :node="node" :depth="depth"/>
+    </ul>
+  </div>
+</template>
