7af06528e660c210ae086363b49aeea44dcd290e
alexinatra2 <a.holzknecht@gmx.de>
2025-01-24 10:28:10 +0100

    refactor coursesStore

--- src/store/courses.ts
+++ src/store/courses.ts
@@ -1,21 +1,34 @@
-import { reactive } from "vue";
+import {reactive} from "vue";
 
-const coursesStore = reactive({
-  courses: [
-    {
-      name: "Kurs 1",
-      id: "II-903",
-    },
-    {
-      name: "Kurs 2",
-      id: "II-912",
-    },
-    {
-      name: "Kurs 3",
-      id: "II-1023",
-    },
-  ],
-  activeCourse: "II-903",
+interface Course {
+    name: string;
+    id: string;
+    desc?: string;
+}
+
+type CoursesStore = {
+    courses: Course[];
+}
+
+const coursesStore = reactive<CoursesStore>({
+    courses: [
+        {
+            name: "Kurs 1",
+            id: "II-903",
+            desc: "Dies ist ein Beispielkurs mit kurzer Beschreibung",
+        },
+        {
+            name: "Kurs 2",
+            id: "II-912",
+            desc: "Dies ist ein Beispielkurs mit langer Beschreibung. " +
+                "Dies ist ein Beispielkurs mit langer Beschreibung. " +
+                "Dies ist ein Beispielkurs mit langer Beschreibung.",
+        },
+        {
+            name: "Kurs 3",
+            id: "II-1023",
+        },
+    ],
 });
 
 export default coursesStore;
