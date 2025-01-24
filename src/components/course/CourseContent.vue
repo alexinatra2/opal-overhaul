da4aadb29b9c2a869b06abb92315cd1f33709a84
alexinatra2 <a.holzknecht@gmx.de>
2025-01-24 22:06:19 +0100

    add course content and change width of left pane

--- src/components/course/CourseContent.vue
+++ src/components/course/CourseContent.vue
@@ -5,7 +5,25 @@ defineProps<{ course: Course }>();
 </script>
 
 <template>
-  <section class="w-full h-full bg-red-500 p-4">
-    <h1 class="text-3xl font-bold">{{ course.name }}</h1>
-  </section>
+  <div class="w-full h-full py-4 px-8 flex flex-col gap-4">
+    <section>
+      <h1 class="text-3xl font-bold">{{ course.name }}</h1>
+      <p class="text-base">{{ course.id }}</p>
+      <p class="text-base text-gray-500">{{ course.author }}</p>
+    </section>
+
+    <section class="flex flex-col gap-2">
+      <h2 class="text-2xl font-bold">Updates</h2>
+      <p>Hier finden Sie updates zu diesem Kurs</p>
+    </section>
+
+    <section class="flex flex-col gap-2">
+      <h2 class="text-2xl font-bold">Beschreibung</h2>
+      <p>{{ course.desc }}</p>
+    </section>
+
+    <section class="">
+
+    </section>
+  </div>
 </template>
