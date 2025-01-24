a76b9f7289db525ed6b78aaee7a7154734fdd850
alexinatra2 <a.holzknecht@gmx.de>
2025-01-24 21:52:02 +0100

    implement horizontal page

--- src/components/shared/OpalHorizontalPage.vue
+++ src/components/shared/OpalHorizontalPage.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import {useSlots} from "vue";
 
+const slots = useSlots();
 </script>
 
 <template>
-  $END$
+  <div class="w-full h-full flex items-stretch">
+    <section class="flex flex-col items-stretch text-left bg-white gap-4">
+      <h1 v-if="slots.headline" class="text-xl font-bold p-4">
+        <slot name="headline"></slot>
+      </h1>
+      <div v-if="slots.sideContent">
+        <slot name="sideContent"></slot>
+      </div>
+    </section>
+    <div class="flex-1">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <style scoped>
