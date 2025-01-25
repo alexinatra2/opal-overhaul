871eae775e2192e4834d5c28d507f8147bbe5d6b
alexinatra2 <a.holzknecht@gmx.de>
2025-01-25 15:45:28 +0100

    fix hover colors of non-default button color variants

--- src/components/shared/OpalButton.vue
+++ src/components/shared/OpalButton.vue
@@ -11,15 +11,15 @@ withDefaults(defineProps<{ colorVariant?: "default" | "green" | "red" }>(), {
 <template>
   <button
       :class="colorVariant === 'red'
-      ? 'bg-secondary-600'
+      ? 'bg-secondary-600 hover:bg-secondary-400'
       : colorVariant === 'green'
-      ? 'bg-success-40'
-      : 'bg-primary-600'"
+      ? 'bg-success-500 hover:bg-success-200'
+      : 'bg-primary-600 hover:bg-primary-400 '"
       class="text-white
   flex items-center gap-4 px-4 py-2
   border-none rounded-full
   cursor-pointer
-  hover:bg-primary-400 transition
+  transition
 ">
     <slot name="start-adornment"></slot>
     <p class="flex-1 font-bold">
