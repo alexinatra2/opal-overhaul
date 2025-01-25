3048b950ba07d555b20b44ab09ea5687e940f5cc
alexinatra2 <a.holzknecht@gmx.de>
2025-01-25 14:52:30 +0100

    add spinner to course search

--- src/components/shared/OpalButton.vue
+++ src/components/shared/OpalButton.vue
@@ -6,7 +6,7 @@ useSlots();
 
 <template>
   <button class="bg-primary-600 text-white
-  flex gap-4 px-4 py-2
+  flex items-center gap-4 px-4 py-2
   border-none rounded-full
   cursor-pointer
   hover:bg-primary-400 transition
