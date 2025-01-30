1d8e965cd253f0e19aa7349707db1820d52c6eee
alexinatra2 <a.holzknecht@gmx.de>
2025-01-30 12:01:24 +0100

    add StartIconButton.vue

---
+++ src/components/shared/StartIconButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import OpalButton from "@/components/shared/OpalButton.vue";
+import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+const props = withDefaults(defineProps<{
+  colorVariant?: "default" | "green" | "red",
+  icon: IconDefinition
+}>(), {
+  colorVariant: "default",
+});
+</script>
+
+<template>
+  <OpalButton v-bind:props>
+    <template #start-adornment>
+      <font-awesome-icon :icon="props.icon"/>
+    </template>
+    <slot></slot>
+  </OpalButton>
+</template>
