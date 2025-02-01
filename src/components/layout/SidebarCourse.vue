9b6214fb59ca70a7bd429b95115206bb54382d6e
maxjon259 <jonathan.bernards@outlook.de>
2025-02-01 17:03:14 +0100

    Refactor SidebarCourse with Tailwind CSS

--- src/components/layout/SidebarCourse.vue
+++ src/components/layout/SidebarCourse.vue
@@ -20,35 +20,10 @@ watch(
 </script>
 
 <template>
-  <router-link :to="`/courses/${props.id}`" class="link">
-    <li class="item" :class="{ 'active': active }">
+  <router-link :to="`/courses/${props.id}`" class="block">
+    <li class="w-72 flex items-center gap-4 p-4 text-gray-800 transition-colors hover:bg-primary-500" :class="{ 'bg-primary-400': active }">
       <h4>{{ props.name }}</h4>
-      <p class="course-id">{{ props.id }}</p>
+      <p class="text-gray-400">{{ props.id }}</p>
     </li>
   </router-link>
 </template>
-
-<style scoped>
-.item {
-  width: 300px;
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  padding: 1rem;
-  list-style: none;
-  color: var(--text-dark);
-  transition: background-color 0.3s ease;
-}
-
-.item:hover {
-  background-color: var(--primary-50);
-}
-
-.active {
-  background-color: var(--primary-40);
-}
-
-.course-id {
-  color: var(--gray-20);
-}
-</style>
